8dbefa6a0ac05d7ffd7d1faf4bdf05c6f4af0d2a
Chris Garrett <cgarrett@linkedin.com>
2019-12-04 11:09:49 -0800

    update references, pull them upstream

--- packages/@ember/-internals/glimmer/lib/helpers/-class.ts
+++ packages/@ember/-internals/glimmer/lib/helpers/-class.ts
@@ -1,22 +1,22 @@
 import { dasherize } from '@ember/string';
-import { VMArguments } from '@glimmer/interfaces';
-import { InternalHelperReference } from '../utils/references';
+import { CapturedArguments, VM, VMArguments } from '@glimmer/interfaces';
+import { HelperRootReference } from '@glimmer/reference';
 
-function classHelper({ positional }: any) {
+function classHelper({ positional }: CapturedArguments) {
   let path = positional.at(0);
   let args = positional.length;
   let value = path.value();
 
   if (value === true) {
     if (args > 1) {
-      return dasherize(positional.at(1).value());
+      return dasherize(positional.at(1).value() as string);
     }
     return null;
   }
 
   if (value === false) {
     if (args > 2) {
-      return dasherize(positional.at(2).value());
+      return dasherize(positional.at(2).value() as string);
     }
     return null;
   }
@@ -24,6 +24,6 @@ function classHelper({ positional }: any) {
   return value;
 }
 
-export default function(args: VMArguments) {
-  return new InternalHelperReference(classHelper, args.capture());
+export default function(args: VMArguments, vm: VM) {
+  return new HelperRootReference(classHelper, args.capture(), vm.env);
 }
